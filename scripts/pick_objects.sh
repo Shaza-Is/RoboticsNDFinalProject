a58d87c20f6646a0bf9491061970394ca7be3970
Shaza-Is <18sika@queensu.ca>
2020-05-20 04:56:03 -0400

    add pick object simulation for pick up and drop off

---
+++ scripts/pick_objects.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sleep 5
+xterm  -e  " source /opt/ros/kinetic/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/shaza/catkin_ws/src/RoboticsNDFinalProject/worlds/building5.world" & 
+sleep 5
+xterm  -e  " source /opt/ros/kinetic/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/shaza/catkin_ws/src/RoboticsNDFinalProject/map/map.yaml initial_pose_a:=4.71" &
+sleep 5
+xterm  -e  " source /opt/ros/kinetic/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5 
+xterm  -e  " source /opt/ros/kinetic/setup.bash; cd catkin_ws; source devel/setup.bash ; rosrun pick_objects pick_objects" &
